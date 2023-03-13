a69b98247769c019921f3c49e0755da8687a524b
josoriom <javier.osorio@correounivalle.edu.co>
2023-03-13 15:51:38 -0500

    chore: setup project

---
+++ setup.ts
@@ -0,0 +1,9 @@
+import * as tf from '@tensorflow/tfjs-node';
+
+export const rootFolder = new URL('../', import.meta.url);
+export const srcFolder = new URL('.', import.meta.url);
+export const modelFolder = new URL('../models/js/', import.meta.url);
+
+export const model = await tf.loadGraphModel(
+    new URL('model.json', modelFolder).href
+);
---
+++ utilities/parseCSV.ts
@@ -0,0 +1,9 @@
+/**
+ * This function returns an array of arrays containing numbers from a CSV file.
+ * @param data the CSV file as string.
+ * @returns the spectra.
+ */
+export function parseCSV(data: string): number[][] {
+  const lines = data.split('\n');
+  return lines.map((line) => line.split(',').map(Number))
+}
